78a2d3bcb9b12397db2ebb6a842184164bda020e
ozkar98 <ozkar.felipe.g@gmail.com>
2018-09-17 17:42:32 -0500

    ActividadEnClase

---
+++ Homework/ComputeBMI.java
@@ -0,0 +1,6 @@
+import java.util.Scanner
+public class ComputeBMI{
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    final double KILOGRAMS_PER_POUND = 0.354592;
+    final d } }
